510c1378dd8ed8cccb82724021eb1f62221d7f8a
pagodingo <47116316+PagoDingo@users.noreply.github.com>
2020-09-05 19:41:04 -0400

    Switched Database to PostgreSQL (Heroku Deploy)

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2020_09_04_032031) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "comments", force: :cascade do |t|
     t.integer "link_id"
     t.text "body"
